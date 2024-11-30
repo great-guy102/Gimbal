43600eb815f6bfc219f65ceadf2a1845998251d5
Wpy <wpy5318280@163.com>
2024-11-30 19:16:38 +0800

    refactor(ins_motor):统一通用参数的设置逻辑、添加初始化标志位

--- Instance/src/ins_motor.cpp
+++ Instance/src/ins_motor.cpp
@@ -1,6 +1,6 @@
 /** 
  *******************************************************************************
- * @file      : ins_motor.cpp
+ * @file      :ins_motor.cpp
  * @brief     : 
  * @history   :
  *  Version     Date            Author          Note
@@ -16,7 +16,7 @@
 #include "ins_motor.hpp"
 /* Private constants ---------------------------------------------------------*/
 
-const hw_motor::OptionalParams kMotorParamsYaw = {
+const hw_motor::OptionalParams kYawMotorParams = {
     .input_type = hw_motor::InputType::kCurr,
     .angle_range = hw_motor::AngleRange::kNegPiToPosPi,
     .dir = hw_motor::kDirFwd,
@@ -28,7 +28,7 @@ const hw_motor::OptionalParams kMotorParamsYaw = {
     // .ex_redu_rat = 14,
 };
 
-const hw_motor::OptionalParams kMotorParamsPitch = {
+const hw_motor::OptionalParams kPitchMotorParams= {
     .input_type = hw_motor::InputType::kCurr,
     .angle_range = hw_motor::AngleRange::kNegPiToPosPi,
     .dir = hw_motor::kDirFwd,  // 设置pitch轴电机低头角度低，抬头角度高
@@ -40,27 +40,16 @@ const hw_motor::OptionalParams kMotorParamsPitch = {
     // .ex_redu_rat = 14,
 };
 
-const hw_motor::OptionalParams kMotorParamsFricLeft = {
+const hw_motor::OptionalParams kFricMotorParams = {
     .input_type = hw_motor::InputType::kRaw,
     .angle_range = hw_motor::AngleRange::kNegPiToPosPi,
-    .dir = hw_motor::kDirFwd,
     /** 是否移除电机自带的减速器 */
     .remove_build_in_reducer = true,
     /** 电机输出端实际角度与规定角度的差值 */
     .angle_offset = 0,
 };
 
-const hw_motor::OptionalParams kMotorParamsFricRight = {
-    .input_type = hw_motor::InputType::kRaw,
-    .angle_range = hw_motor::AngleRange::kNegPiToPosPi,
-    .dir = hw_motor::kDirRev,
-    /** 是否移除电机自带的减速器 */
-    .remove_build_in_reducer = true,
-    /** 电机输出端实际角度与规定角度的差值 */
-    .angle_offset = 0,
-};
-
-const hw_motor::OptionalParams kMotorParamsFeed = {
+const hw_motor::OptionalParams kFeedMotorParams = {
     // feed轮是3508电机
     .input_type = hw_motor::InputType::kCurr,
     .angle_range = hw_motor::AngleRange::kNegPiToPosPi,
@@ -73,29 +62,74 @@ const hw_motor::OptionalParams kMotorParamsFeed = {
     .ex_redu_rat = 51,
 };
 
-enum MotorID {
-  kMotorIdFricRight = 1u,
-  kMotorIdFricLeft = 2u,
-  kMotorIdYaw = 3u,
-  kMotorIdPitch = 4u,
-  kMotorIdFeed = 5u,
-};
 /* Private macro -------------------------------------------------------------*/
 /* Private types -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-
-hw_motor::DM_J4310 unique_motor_yaw = hw_motor::DM_J4310(kMotorIdYaw, kMotorParamsYaw);
-hw_motor::DM_J4310 unique_motor_pitch = hw_motor::DM_J4310(kMotorIdPitch, kMotorParamsPitch);
-hw_motor::M3508 unique_motor_fric_left = hw_motor::M3508(kMotorIdFricLeft, kMotorParamsFricLeft);
-hw_motor::M3508 unique_motor_fric_right = hw_motor::M3508(kMotorIdFricRight, kMotorParamsFricRight);
-hw_motor::M3508 unique_motor_feed = hw_motor::M3508(kMotorIdFeed, kMotorParamsFeed);
 /* External variables --------------------------------------------------------*/
+
+//yaw电机单例
+static bool is_motor_yaw_inited = false;
+hw_motor::DM_J4310 unique_motor_yaw;
+
+//pitch电机单例
+static bool is_motor_pitch_inited = false;
+hw_motor::DM_J4310 unique_motor_pitch;
+
+//摩擦轮电机单例
+static bool is_motor_fric_left_inited = false;
+hw_motor::M3508 unique_motor_fric_left;
+
+static bool is_motor_fric_right_inited = false;
+hw_motor::M3508 unique_motor_fric_right;
+
+//feed轮电机单例
+static bool is_motor_feed_inited = false;
+hw_motor::M3508 unique_motor_feed;
+
 /* Private function prototypes -----------------------------------------------*/
 /* Exported function definitions ---------------------------------------------*/
 
-hw_motor::Motor* GetMotorYaw() { return &unique_motor_yaw; };
-hw_motor::Motor* GetMotorPitch() { return &unique_motor_pitch; };
-hw_motor::Motor* GetMotorFricLeft() { return &unique_motor_fric_left; };
-hw_motor::Motor* GetMotorFricRight() { return &unique_motor_fric_right; };
-hw_motor::Motor* GetMotorFeed() { return &unique_motor_feed; };
+hw_motor::Motor* GetMotorYaw() { 
+    if(!is_motor_yaw_inited){
+        unique_motor_yaw = hw_motor::DM_J4310(0x01, kYawMotorParams);
+        is_motor_yaw_inited = true;
+    }
+    return &unique_motor_yaw;
+};
+
+hw_motor::Motor* GetMotorPitch() { 
+    if(!is_motor_pitch_inited){
+        unique_motor_pitch = hw_motor::DM_J4310(0x02, kPitchMotorParams);
+        is_motor_pitch_inited = true;
+    }
+    return &unique_motor_pitch;
+};
+hw_motor::Motor* GetMotorFricLeft() { 
+    if(!is_motor_fric_left_inited){
+        hw_motor::OptionalParams FricMotorParamsLeft = kFricMotorParams;
+        FricMotorParamsLeft.dir = hw_motor::kDirFwd;
+        unique_motor_fric_left = hw_motor::M3508(0x01, FricMotorParamsLeft);
+        is_motor_fric_left_inited = true;
+    }
+    return &unique_motor_fric_left; 
+};
+
+hw_motor::Motor* GetMotorFricRight() { 
+    if(!is_motor_fric_right_inited){
+        hw_motor::OptionalParams FricMotorParamsRight = kFricMotorParams;
+        FricMotorParamsRight.dir = hw_motor::kDirRev;
+        unique_motor_fric_right = hw_motor::M3508(0x02, FricMotorParamsRight);
+        is_motor_fric_right_inited = true;
+    }
+    return &unique_motor_fric_right; 
+};
+
+hw_motor::Motor* GetMotorFeed() { 
+    if(!is_motor_feed_inited){
+        unique_motor_feed = hw_motor::M3508(0x03, kFeedMotorParams);
+        is_motor_feed_inited = true;
+    }
+    return &unique_motor_feed; 
+};
+
 /* Private function definitions ----------------------------------------------*/
